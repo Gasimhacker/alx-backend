0824987a1030ac016d02b91d3e981194003b3fd5
Mohamed Ahmed <ma2286572@gmail.com>
2024-07-30 23:33:18 +0000

    Fix pycodestyle error

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -10,7 +10,8 @@ class Config:
     """A configuration class for a flask app"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC" 
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
