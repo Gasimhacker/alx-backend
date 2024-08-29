ef9fcc1eeb146616e9ab95e7e21905369bbcf140
Mohamed Ahmed <ma2286572@gmail.com>
2024-08-29 12:22:02 +0000

    Write the job creation function

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,17 @@
+export default function createPushNotificationsJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
+
+  for (let i = 0; i < jobs.length; i += 1) {
+    const job = queue.create('push_notification_code_3', jobs[i]).save((err) => {
+      if (!err) console.log(`Notification job created: ${job.id}`);
+    });
+
+    job.on('complete', () => console.log(`Notification job ${job.id} completed`));
+
+    job.on('failed', (err) => console.log(`Notification job ${job.id} failed: ${err}`));
+
+    job.on('progress', (progress) => console.log(`Notification job ${job.id} ${progress}% complete`));
+  }
+}
