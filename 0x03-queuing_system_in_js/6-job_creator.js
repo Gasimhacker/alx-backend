2c9608c7287cfa28872e7104a9fbce19cce8d83f
Mohamed Ahmed <ma2286572@gmail.com>
2024-08-29 10:09:08 +0000

    Create a the Job processor

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -3,8 +3,8 @@ const kue = require('kue');
 const queue = kue.createQueue();
 
 const job = queue.create('push_notification_code', {
-  phoneNumber: '0123456789',
-  message: 'This is a message',
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
 }).save((err) => {
   if (!err) console.log(`Notification job created: ${job.id}`);
 });
